cbab5054fb8ebeda2d87b3e52a597cbfa89cd990
koushik-j-s <koushikskoushiks5@gmail.com>
2024-08-31 12:37:49 +0530

    Add files via upload

---
+++ stack in structure.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<limits.h>
+#include<stdlib.h>
+
+#define MAX_SIZE 20
+
+struct Stack{
+    int *arr;
+    int top;
+    int size;
+};
+void initStack(struct Stack *stack,int size){
+  stack->arr = (int *)malloc(size * sizeof(int));
+  stack->top = -1;
+  stack->size = size;
+}
+
+void push(struct Stack *stack,int val){
+  if(stack->top == stack->size - 1){
+    printf("Stack is Full\n");
+    return;
+  }
+  stack->arr[++stack->top] = val;
+}
+int pop(struct Stack *stack){
+  if(stack->top == -1){
+    printf("Stack is Empty\n");
+    return INT_MIN;
+}
+return stack->arr[stack->top--];
+}
+int peek(struct Stack *stack){
+  if(stack->top == -1){
+    printf("Stack is Empty\n");
+    return INT_MIN;
+  }
+  return stack->arr[stack->top];
+}
+void freeStack(struct Stack *stack){
+  free(stack->arr);
+}
+int main(){
+  struct Stack stack1, stack2;
+  initStack(&stack1, MAX_SIZE);
+  initStack(&stack2, MAX_SIZE);
+
+  push(&stack1, 10);
+  push(&stack1, 20);
+  push(&stack1, 30);
+  push(&stack1, 40);
+  push(&stack1, 50);
+
+  push(&stack2,100);
+  push(&stack2,200);
+  push(&stack2,300);
+
+  printf("Poppedd element from stack1: %d\n",pop(&stack1));
+  printf("Poppedd element from stack1: %d\n",pop(&stack1));
+
+  printf("Top of stack2: %d\n",peek(&stack2));
+
+  freeStack(&stack1);
+  freeStack(&stack2);
+
+  return 0;
+}
+
